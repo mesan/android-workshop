31d2db160c90172f5d2c69e8fdbea7800166dc1e
tpettersen <tpettersen@2105e491-9f47-425f-a337-16d41816b06d>
2010-08-19 08:25:49 +0000

    Databasen ferdig
    git-svn-id: http://scrum.unfuddle.com/svn/scrum_mesan@3 2105e491-9f47-425f-a337-16d41816b06d

--- MesanWorkshopTwitterClient/src/com/mesan/android/demo/model/data/TwitterDAO.java
+++ MesanWorkshopTwitterClient/src/com/mesan/android/demo/model/data/TwitterDAO.java
@@ -1,7 +1,6 @@
 package com.mesan.android.demo.model.data;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import android.content.Context;
 import android.util.Log;
@@ -17,94 +16,94 @@ public class TwitterDAO {
 
 	private Context context;
 	private static ObjectContainer oc = null;
-	
-	public TwitterDAO(Context context){
+	private static final String PRIMARY_KEY = "keyword";
+
+	public TwitterDAO(Context context) {
 		this.context = context;
 	}
-    
-    public ObjectContainer db(){
-    	try {
-    		if(oc == null || oc.ext().isClosed()){
-    			oc = Db4o.openFile(dbConfig(), db4oDBFullPath(context));
-    		}
-    		return oc;
-    	} catch (Exception e) {
-        	Log.e(TwitterDAO.class.getName(), e.toString());
-        	return null;
-        }
-    }
-    
-    private Configuration dbConfig(){    	
-    	Configuration c = Db4o.newConfiguration();    	
-    	c.objectClass(TwitterDTO.class).objectField("keyword").indexed(true); 
-    	c.objectClass(TwitterDTO.class).updateDepth(6);
-    	c.objectClass(TwitterDTO.class).minimumActivationDepth(6);
-    	c.objectClass(TwitterDTO.class).cascadeOnDelete(true);    	
-    	return c;
-    }
-	
+
+	public ObjectContainer db() {
+		try {
+			if (oc == null || oc.ext().isClosed()) {
+				oc = Db4o.openFile(dbConfig(), db4oDBFullPath(context));
+			}
+			return oc;
+		} catch (Exception e) {
+			Log.e(TwitterDAO.class.getName(), e.toString());
+			return null;
+		}
+	}
+
+	private Configuration dbConfig() {
+		Configuration c = Db4o.newConfiguration();
+		c.objectClass(TwitterDTO.class).objectField(PRIMARY_KEY).indexed(true);
+		c.objectClass(TwitterDTO.class).updateDepth(6);
+		c.objectClass(TwitterDTO.class).minimumActivationDepth(6);
+		c.objectClass(TwitterDTO.class).cascadeOnDelete(true);
+		return c;
+	}
+
 	private String db4oDBFullPath(Context ctx) {
 		return ctx.getDir("data", 0) + "/" + "browsemap.db4o";
 	}
-	
+
 	/**
-     * Close database connection
-     */
-    public void close() {
-    	if(oc != null)
-    		oc.close();
-    }
-    
-    /*public void setBookmark(String name, String description){    	
-    	TwitterDTO bookmark = getTweet(name);
-    	
-    	if(bookmark == null){
-    		bookmark = new MapBookmark(name);
-    	}
-    	bookmark.setDescription(description);
+	 * Close database connection
+	 */
+	public void close() {
+		if (oc != null)
+			oc.close();
+	}
 
-    	db().store(bookmark);
-    	db().commit();
-    }
-    
-    public MapBookmark getBookmark(String name){
-    	MapBookmark bookmark = new MapBookmark(name);
-    	ObjectSet<MapBookmark> result = db().queryByExample(bookmark);
-    	
-    	if(result.hasNext()){
-    		return (MapBookmark)result.next();
-    	}
-    	return null;
-    }
-    
-    @SuppressWarnings("unchecked")
-	public List<MapBookmark> getBookmarkList(){
-    	ArrayList<MapBookmark> ret = new ArrayList<MapBookmark>();
-        ObjectSet result = getBookmarks();
-        while (result.hasNext())
-        	ret.add((MapBookmark)result.next());
-        return ret;
-    }
-    
-    @SuppressWarnings("unchecked")
-	private ObjectSet getBookmarks(){
-    	Query query = db().query();
-    	query.constrain(MapBookmark.class);
-    	query.descend("name").orderAscending();
-    	return query.execute();
-    }
+	public void setTweet(TwitterDTO twitterDTO) {
+		TwitterDTO tempTweet = getTweet(twitterDTO.getKeyword());
 
-    public void deleteBookmark(String name) {
-        //Search by name
-    	MapBookmark bkm = getBookmark(name);
-        //Delete object
-    	if(bkm != null){
-    		db().delete(bkm);
-    		db().commit();
-    	}
-    }
-    
-    public int bookamrkCount(){
-    	return getBookmarks().size();
-    }*/
+		if (tempTweet == null) {
+			tempTweet = new TwitterDTO(twitterDTO.getKeyword());
+		}
+		tempTweet.setTweets(twitterDTO.getTweets());
+
+		db().store(tempTweet);
+		db().commit();
+	}
+
+	public TwitterDTO getTweet(String keyword) {
+		TwitterDTO twitterDTO = new TwitterDTO(keyword);
+		ObjectSet<TwitterDTO> result = db().queryByExample(twitterDTO);
+
+		if (result.hasNext()) {
+			return (TwitterDTO) result.next();
+		}
+		return null;
+	}
+
+	public ArrayList<TwitterDTO> getTweets() {
+		ArrayList<TwitterDTO> ret = new ArrayList<TwitterDTO>();
+		ObjectSet<TwitterDTO> result = getAllTweets();
+		while (result.hasNext())
+			ret.add((TwitterDTO) result.next());
+		return ret;
+	}
+
+	@SuppressWarnings("unchecked")
+	private ObjectSet<TwitterDTO> getAllTweets() {
+		Query query = db().query();
+		query.constrain(TwitterDTO.class);
+		query.descend(PRIMARY_KEY).orderAscending();
+		return query.execute();
+	}
+
+	public void deleteTweet(String keyword) {
+		// Search by name
+		TwitterDTO twitterDTO = getTweet(keyword);
+		// Delete object
+		if (twitterDTO != null) {
+			db().delete(twitterDTO);
+			db().commit();
+		}
+	}
+
+	public int bookamrkCount() {
+		return getAllTweets().size();
+	}
 }
