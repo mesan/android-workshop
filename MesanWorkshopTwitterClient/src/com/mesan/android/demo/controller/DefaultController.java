00974636fb1fb628fcad36fc2d98fcab4062d4fc
rran <rran@2105e491-9f47-425f-a337-16d41816b06d>
2010-09-01 08:00:18 +0000

    git-svn-id: http://scrum.unfuddle.com/svn/scrum_mesan@25 2105e491-9f47-425f-a337-16d41816b06d

--- MesanWorkshopTwitterClient/src/com/mesan/android/demo/controller/DefaultController.java
+++ MesanWorkshopTwitterClient/src/com/mesan/android/demo/controller/DefaultController.java
@@ -3,13 +3,10 @@ package com.mesan.android.demo.controller;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,28 +19,28 @@ import com.mesan.android.demo.model.dto.TwitterDTO;
 import com.mesan.android.demo.model.util.TwitterUtil;
 
 public class DefaultController extends Activity {
-	
+
 	private EditText txtKeyword;
 	private Button btnSearch;
 	private ListView lstKeywords;
-	
+
 	private ArrayList<String> keywords;
 	private TwitterUtil twitterUtil;
-	
+
 	private Context context;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);               
-        
-        context = this;
-        
-        initLayout();
-        renderView();
-        initListeners();
-        
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		context = this;
+
+		initLayout();
+		renderView();
+		initListeners();
+
+	}
 
 	private void initLayout() {
 		txtKeyword = (EditText) findViewById(R.id.txtKeyword);
@@ -51,20 +48,19 @@ public class DefaultController extends Activity {
 		lstKeywords = (ListView) findViewById(R.id.lstKeywords);
 	}
 
-
 	private void renderView() {
 		keywords = new ArrayList<String>();
-		twitterUtil = new TwitterUtil(context);	
+		twitterUtil = new TwitterUtil(context);
 		populateList();
 	}
 
 	private void initListeners() {
-		
+
 		btnSearch.setOnClickListener(new View.OnClickListener() {
-			
+
 			public void onClick(View v) {
 				String keyword = txtKeyword.getText().toString();
-				if(!"".equals(keyword)) {					
+				if (!"".equals(keyword)) {
 					txtKeyword.setText("");
 					Application.hideKeyboard(context, txtKeyword);
 					goToActivity(keyword, context);
@@ -72,36 +68,55 @@ public class DefaultController extends Activity {
 			}
 		});
 		
-		lstKeywords.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		txtKeyword.setOnKeyListener(new View.OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
 
-			public void onItemClick(AdapterView<?> arg0, View view, int pos, long id) {
-				goToActivity(keywords.get(pos), view.getContext());
+				// If ENTER is pressed
+				if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+					String keyword = txtKeyword.getText().toString();
+					if (!"".equals(keyword)) {
+						txtKeyword.setText("");
+						Application.hideKeyboard(context, txtKeyword);
+						goToActivity(keyword, context);
+						return true;
+					}
+				}
+				return false;
 			}
 		});
+
+		lstKeywords
+				.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+					public void onItemClick(AdapterView<?> arg0, View view,
+							int pos, long id) {
+						goToActivity(keywords.get(pos), view.getContext());
+					}
+				});
 	}
-	
-	private void populateList(){
+
+	private void populateList() {
 		keywords.clear();
 		ArrayList<TwitterDTO> allTweets = twitterUtil.getAllTwitterDTOs();
-		for (TwitterDTO t : allTweets){
+		for (TwitterDTO t : allTweets) {
 			keywords.add(t.getKeyword());
 		}
-		lstKeywords.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, keywords));
+		lstKeywords.setAdapter(new ArrayAdapter<String>(context,
+				android.R.layout.simple_list_item_1, keywords));
 	}
-	
-	private void goToActivity(String keyword, Context context){
+
+	private void goToActivity(String keyword, Context context) {
 		Intent myIntent = new Intent();
-		
+
 		myIntent.setClass(context, TweetsController.class);
 		myIntent.putExtra("keyword", keyword);
 		startActivity(myIntent);
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		populateList();
 	}
-	
 
 }
