40bc93bbb12d07f8b9bfd63593e4b9d0fcf8408d
Thomas Pettersen <tpettersen@gmail.com>
2015-08-16 14:46:25 +0200

    init

--- AndroidWorkshop/app/src/main/java/no/mesan/mobil/androidworkshop/view/NavigationDrawerFragment.java
+++ AndroidWorkshop/app/src/main/java/no/mesan/mobil/androidworkshop/view/NavigationDrawerFragment.java
@@ -1,4 +1,4 @@
-package no.mesan.mobil.androidworkshop;
+package no.mesan.mobil.androidworkshop.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
@@ -21,6 +21,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import no.mesan.mobil.androidworkshop.R;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
