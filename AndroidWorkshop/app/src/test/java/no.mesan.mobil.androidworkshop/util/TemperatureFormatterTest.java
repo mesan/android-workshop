3b068bf0d827936c74a291df7b2b549bd7170254
Thomas Pettersen <tpettersen@gmail.com>
2015-08-30 20:26:04 +0200

    Added tests for oppgave4

---
+++ AndroidWorkshop/app/src/test/java/no.mesan.mobil.androidworkshop/util/TemperatureFormatterTest.java
@@ -0,0 +1,38 @@
+package no.mesan.mobil.androidworkshop.util;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/* For useful information, visit
+https://developer.android.com/training/testing/unit-testing/local-unit-tests.html
+ */
+
+// Oppgave 4c
+public class TemperatureFormatterTest {
+
+    @Test
+    public void temperatureFormatter_PositiveTemperature() {
+
+    }
+
+    @Test
+    public void temperatureFormatter_NegativeTemperature() {
+
+    }
+
+    @Test
+    public void temperatureFormatter_ZeroTemperature() {
+
+    }
+
+    @Test
+    public void temperatureFormatter_TwoDecimalsRoundUp() {
+
+    }
+
+    @Test
+    public void temperatureFormatter_TwoDecimalsRoundDown() {
+
+    }
+}
