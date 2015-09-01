c19a67b9468aa9f2feb8cf9a4a28a93f07803a55
Anders Ullnæss <andersu@Mac-and-cheese.local>
2015-09-01 11:45:31 +0200

    Added hideKeyboard methiod to oppgave1

--- AndroidWorkshop/app/src/main/java/no/mesan/mobil/androidworkshop/view/main/SearchFragment.java
+++ AndroidWorkshop/app/src/main/java/no/mesan/mobil/androidworkshop/view/main/SearchFragment.java
@@ -1,10 +1,12 @@
 package no.mesan.mobil.androidworkshop.view.main;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import no.mesan.mobil.androidworkshop.R;
 
@@ -33,5 +35,14 @@ public class SearchFragment extends Fragment {
     private void initListeners() {
         // Oppgave 1c - Lage lytter for søkeknapp som viser snackbar
 
+    }
+
+    private void hideKeyboard() {
+        // Check if no view has focus:
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
